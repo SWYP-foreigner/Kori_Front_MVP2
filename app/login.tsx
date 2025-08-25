aef3534fed0bce3297c79c930890cad7c09bc2c3
Shinhyo1216 <tlsgyals7632@naver.com>
2025-08-26 08:51:25 +0900

    develop login

--- app/login.tsx
+++ app/login.tsx
@@ -1,20 +1,18 @@
-import React , { useEffect ,useState} from 'react';
-import { Alert } from "react-native";
+import React, { useState } from 'react';
 import styled from 'styled-components/native';
 // import * as SecureStore from 'expo-secure-store'; // JWT 토큰을 저장하기 위함
-import { useRouter } from 'expo-router';
-import GoogleSignInButton from '@/components/GoogleSignInButton';
 import AppleSignInButton from '@/components/AppleSignInButton';
+import GoogleSignInButton from '@/components/GoogleSignInButton';
 import {
-  GoogleSignin,
-  isSuccessResponse,
-  statusCodes,
-  isErrorWithCode
+  GoogleSignin, isErrorWithCode, isSuccessResponse,
+  statusCodes
 } from '@react-native-google-signin/google-signin';
 import axios from "axios";
+import { useRouter } from 'expo-router';
 
 GoogleSignin.configure({
-  "webClientId":"86972168076-3bllmjnmkf9o6o7puri902co61jonbmi.apps.googleusercontent.com",
+  "webClientId": "86972168076-3bllmjnmkf9o6o7puri902co61jonbmi.apps.googleusercontent.com",
+  "iosClientId": "86972168076-m7l8vrcmav3v3pofhu6ssheq39s9kvht.apps.googleusercontent.com",
   offlineAccess: true
 });
 
@@ -22,8 +20,8 @@ const LoginScreen = () => {
   const router = useRouter();
   const [userInfo, setUserInfo] = useState(null);
 
-  const sendTokenToServer=async(code)=>{
-      try {
+  const sendTokenToServer = async (code) => {
+    try {
       const res = await axios.post('https://dev.ko-ri.cloud/api/v1/member/google/app-login', {
         "code": code
       });
@@ -33,67 +31,67 @@ const LoginScreen = () => {
       console.error('서버 요청 실패', error);
     }
   }
-  
-const signIn = async () => {
-  try {
-    await GoogleSignin.hasPlayServices();
-    const response = await GoogleSignin.signIn();
-    if (isSuccessResponse(response)) {
-      console.log('들어옴');
-      setUserInfo({ userInfo: response.data });
-      console.log(response.data);
-      const code=response.data.serverAuthCode;
-      sendTokenToServer(code);
-    } else {
-      console.log('sign in was cancelled by user');
-      // sign in was cancelled by user
-    }
-  } catch (error) {
-    if (isErrorWithCode(error)) {
-      switch (error.code) {
-        case statusCodes.IN_PROGRESS:
-          console.log('operation (eg. sign in) already in progress');
-          // operation (eg. sign in) already in progress
-          break;
-        case statusCodes.PLAY_SERVICES_NOT_AVAILABLE:
-          console.log('Android only, play services not available or outdated');
-          // Android only, play services not available or outdated
-          break;
-        default:
-        // some other error happened
-      }
-    } else {
-      console.log('error');
-      // an error that's not related to google sign in occurred
-    }
-  }
-};
 
-const signOut = async () => {
-  try {
-    await GoogleSignin.signOut();
-    setUserInfo({ user: null }); // Remember to remove the user from your app's state as well
-    console.log('로그아웃');
-  } catch (error) {
-    console.error(error);
-  }
-};
+  const signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const response = await GoogleSignin.signIn();
+      if (isSuccessResponse(response)) {
+        console.log('들어옴');
+        setUserInfo({ userInfo: response.data });
+        console.log(response.data);
+        const code = response.data.serverAuthCode;
+        sendTokenToServer(code);
+      } else {
+        console.log('sign in was cancelled by user');
+        // sign in was cancelled by user
+      }
+    } catch (error) {
+      if (isErrorWithCode(error)) {
+        switch (error.code) {
+          case statusCodes.IN_PROGRESS:
+            console.log('operation (eg. sign in) already in progress');
+            // operation (eg. sign in) already in progress
+            break;
+          case statusCodes.PLAY_SERVICES_NOT_AVAILABLE:
+            console.log('Android only, play services not available or outdated');
+            // Android only, play services not available or outdated
+            break;
+          default:
+          // some other error happened
+        }
+      } else {
+        console.log('error');
+        // an error that's not related to google sign in occurred
+      }
+    }
+  };
+
+  const signOut = async () => {
+    try {
+      await GoogleSignin.signOut();
+      setUserInfo({ user: null }); // Remember to remove the user from your app's state as well
+      console.log('로그아웃');
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <Container>
       <GoogleSignInButton
         // disabled={!request}
-       onPress={signIn}
+        onPress={signIn}
       />
-      <AppleSignInButton /> 
-      <TabsMoveButton  onPress={signOut}>
+      <AppleSignInButton />
+      <TabsMoveButton onPress={signOut}>
         <TabsMoveText>구글 Sign out</TabsMoveText>
       </TabsMoveButton>
       {/* 테스트용 / 로그인 없이 탭 이동 버튼 */}
       <TabsMoveButton onPress={() => router.push('./(tabs)')}>
         <TabsMoveText>Tabs 화면으로 이동</TabsMoveText>
       </TabsMoveButton>
-      
+
       <ProfileMoveButton onPress={() => router.push('./screens/makeprofile/NameStepScreen')}>
         <ProfileMoveText>프로필 등록 화면으로 이동</ProfileMoveText>
       </ProfileMoveButton>
--- app/screens/chatscreen/ChattingRoomScreen.tsx
+++ app/screens/chatscreen/ChattingRoomScreen.tsx
@@ -22,6 +22,7 @@ const ChattingRoomScreen=()=>{
                             <SimpleLineIcons name="menu" size={20} color="#CCCFD0"  style={{ marginLeft: 10 }}  />
                         </Right>
                 </HeaderContainer>
+                <ChatScreenContainer>
                 <TimeView>
                     <TimeText>2025.08.15(Fri)</TimeText>
                 </TimeView>
@@ -40,7 +41,49 @@ const ChattingRoomScreen=()=>{
                             <OtherText>Hola~ Vine a Corea desde Estados Unidos como estudiante de intercambio</OtherText>
                         </OtherNotFirstTextBox>
                     </OtherContainer>
+                    <TimeTextContainer>
+                        <ChatTimeText>16:30</ChatTimeText>
+                    </TimeTextContainer>
                 </ChattingLeftContainer>
+                <ChattingRightContainer>
+                    <TimeTextContainer>
+                        <ChatTimeText>08:30</ChatTimeText>
+                    </TimeTextContainer>
+                    <MyTextContainer>
+                        <MyText>I think</MyText>
+                    </MyTextContainer>
+                </ChattingRightContainer>
+                <ChatDivider/>
+                <TimeView>
+                    <TimeText>2025.08.15(Fri)</TimeText>
+                </TimeView>
+                 <ChattingLeftContainer>
+                    <ProfileContainer>
+                        <ProfileBox>
+                            <ProfileImage source={require("@/assets/images/character2.png")}/>
+                        </ProfileBox>
+                    </ProfileContainer>
+                    <OtherContainer>
+                        <OtherNameText>Kori</OtherNameText>
+                        <OtherFirstTextBox>
+                            <OtherText>Hola~</OtherText>
+                        </OtherFirstTextBox>
+                        <OtherNotFirstTextBox>
+                            <OtherText>Hola~ Vine a Corea desde Estados Unidos como estudiante de intercambio</OtherText>
+                        </OtherNotFirstTextBox>
+                    </OtherContainer>
+                    <TimeTextContainer>
+                        <ChatTimeText>16:30</ChatTimeText>
+                    </TimeTextContainer>
+                </ChattingLeftContainer>
+                </ChatScreenContainer>
+                <BottomContainer>
+                    <InputTextBox
+                        placeholder="Write Your Text"
+                        placeholderTextColor="#616262"
+                    />
+                    <Feather name="send" size={23} color="#ffffff" />
+                </BottomContainer>
             </Container>
         </SafeArea>
 
@@ -60,6 +103,7 @@ const Container=styled.View`
     flex:1;
     background-color:#1D1E1F;
     padding:0px 15px;
+    
 `;
 
 const HeaderContainer=styled.View`
@@ -94,10 +138,19 @@ const Right=styled.View`
     justify-content:center;
 
 `;
+
+const ChatScreenContainer=styled.View`
+    background-color:blue;
+    flex-direction:column;
+    height:76%;
+`;
+
 const TimeView=styled.View`
     background-color:green;
     align-items:center;
     justify-content:center;
+    height:20px;
+    margin:20px 0px;
 `;
 const TimeText=styled.Text`
     color:#848687;
@@ -106,8 +159,8 @@ const TimeText=styled.Text`
 `;
 const ChattingLeftContainer = styled.View`
   background-color: yellow;
-  max-width:280px;   /* 최대 너비 */
-  height: 152px;
+  max-width:285px;   /* 최대 너비 */
+ 
   flex-direction: row;
 `;
 
@@ -137,27 +190,93 @@ const OtherNameText=styled.Text`
     font-family:PlusJakartaSans_600SemiBold;
     font-size:13px;
 `;
-const OtherFirstTextBox=styled.View`
-    background-color:#414142;
-    max-width:210px;
-    margin-top:5px;
-    border-top-left-radius: 0px;   /* ⬅️ 왼쪽 상단만 0 */
-    border-top-right-radius: 16px;
-    border-bottom-left-radius: 16px;
-    border-bottom-right-radius: 16px;
+const OtherFirstTextBox = styled.View`
+  background-color: #414142;
+  padding: 10px 14px;              /* 안쪽 여백 */
+  margin-top: 5px;
+  border-top-left-radius: 0px;
+  border-top-right-radius: 16px;
+  border-bottom-left-radius: 16px;
+  border-bottom-right-radius: 16px;
+
+  align-self: flex-start;          /* 텍스트 크기에 맞게 좌측 정렬 */
+  max-width: 210px;                /* 최대 너비 */
 `;
+
 const OtherText=styled.Text`
     color:#FFFFFF;
     font-size:14px;
     font-family:PlusJakartaSans_300Light;
 `;
 const OtherNotFirstTextBox=styled.View`
-    margin-top:10px;
-    background-color:#414142;
-    max-width:210px;
-    padding:10px 12px;;
-    border-radius:10px;
-    align-items:center;
-    justify-content:center;
+  background-color: #414142;
+  padding: 10px 14px;              /* 안쪽 여백 */
+  margin-top: 5px;
+  border-radius:16px;
+  align-self: flex-start;          /* 텍스트 크기에 맞게 좌측 정렬 */
+  max-width: 210px;                /* 최대 너비 */
 `;
-const OtherTimeText=styled.View``;
+
+const TimeTextContainer=styled.View`
+    background-color:red;
+    width:32px;
+    align-items:center; 
+    justify-content:flex-end;
+    margin : 0px 3px;
+`;
+const ChatTimeText=styled.Text`
+    color:#848687;
+    font-family:PlusJakartaSans_300Light;
+    font-size:10px;
+`;
+
+const ChattingRightContainer = styled.View`
+  align-self: flex-end;      /* 부모 기준 오른쪽 정렬 */
+  max-width: 285px;
+  background-color: yellow;
+  margin-top:20px;       /* 위/아래 간격 */
+  margin-right:10px;
+  flex-direction:row;
+`;
+
+const MyTextContainer=styled.View`
+  padding: 10px 14px;        /* 말풍선 안쪽 여백 */
+  max-width: 285px;
+  border-top-left-radius: 16px;
+  border-top-right-radius: 16px;
+  border-bottom-left-radius: 16px;
+  border-bottom-right-radius: 0px;
+  background-color:#02F59B;
+`;
+const MyText=styled.Text`
+    color:#1D1E1F;
+    font-size:14px;
+    font-family: PlusJakartaSans_400Regular;
+`;
+
+const ChatDivider=styled.View`
+    width:100%;
+    height:4px;
+    background-color:#35363799;
+    margin:20px 0px;
+`;
+
+const BottomContainer=styled.View`
+    background-color:#1D1E1F;
+    height:60px;
+    border-top-width: 1px;
+    border-top-color: #353637;
+    justify-content:center;
+    flex-direction:row;
+    align-items:center;
+
+`;
+const InputTextBox=styled.TextInput`
+    margin-left:5px;
+    background-color:#353637;
+    width:85%;
+    height:45px;
+    border-radius:8px;
+    margin-right:30px;
+    margin-left:-5px;
+`;
