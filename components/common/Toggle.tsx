0e4fe813e9758807535d76c6334745f70a716d01
shinhm1 <shinhm1@naver.com>
2025-10-17 05:45:00 +0900

    style: toggle 컴포넌트 disabled 속성 추가

--- components/common/Toggle.tsx
+++ components/common/Toggle.tsx
@@ -4,43 +4,47 @@ import styled from 'styled-components/native';
 
 interface ToggleProps {
   isPressed: boolean;
-  onPress: (value: boolean) => void;
+  onPress: (_value: boolean) => void;
+  disabled?: boolean;
 }
 
-const Toggle = ({ isPressed, onPress }: ToggleProps) => {
+const Toggle = ({ isPressed, onPress, disabled = false }: ToggleProps) => {
   const offsetX = useRef(new Animated.Value(isPressed ? 20 : 0)).current;
 
+  // TODO 애니메이션 적용 안되는 문제 해결 필요
   useEffect(() => {
     Animated.timing(offsetX, {
       toValue: isPressed ? 20 : 0,
       duration: 200,
-      useNativeDriver: false,
+      useNativeDriver: true,
     }).start();
   }, [isPressed]);
 
   return (
     <TouchableWithoutFeedback onPress={() => onPress(!isPressed)}>
-      <Background isPressed={isPressed}>
-        <AnimatedSwitch style={{ transform: [{ translateX: offsetX }] }} />
+      <Background isPressed={isPressed} disabled={disabled}>
+        <AnimatedSwitch disabled={disabled} style={{ transform: [{ translateX: offsetX }] }} />
       </Background>
     </TouchableWithoutFeedback>
   );
 };
 
-const Background = styled.View<{ isPressed: boolean }>`
+const Background = styled.View<{ isPressed: boolean; disabled: boolean }>`
   justify-content: center;
   border-radius: 40px;
   width: 44px;
   height: 24px;
   padding: 2px;
-  background-color: ${({ theme, isPressed }) => (isPressed ? theme.colors.primary.mint : theme.colors.gray.darkGray_2)};
+
+  background-color: ${({ theme, isPressed, disabled }) =>
+    disabled ? theme.colors.gray.darkGray_1 : isPressed ? theme.colors.primary.mint : theme.colors.gray.darkGray_2};
 `;
 
-const AnimatedSwitch = styled(Animated.View)`
+const AnimatedSwitch = styled(Animated.View)<{ disabled: boolean }>`
   width: 20px;
   height: 20px;
   border-radius: 40px;
-  background-color: ${({ theme }) => theme.colors.primary.white};
+  background-color: ${({ theme, disabled }) => (disabled ? theme.colors.gray.darkGray_2 : theme.colors.primary.white)};
 `;
 
 export default Toggle;
