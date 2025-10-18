62fac047e80ff8ad703a1a40de40a078f23f9fa5
shinhm1 <shinhm1@naver.com>
2025-10-18 19:43:46 +0900

    feat: useNotification hooks에 placeholderData 추가

--- hooks/queries/notifications/useNotifications.ts
+++ hooks/queries/notifications/useNotifications.ts
@@ -1,10 +1,21 @@
 import { getNotificationsSettingStatus, NotificationSetting } from '@/api/notifications/notifications';
 import { useQuery } from '@tanstack/react-query';
 
+export const defaultSettings: NotificationSetting[] = [
+  { notificationType: 'post', enabled: false },
+  { notificationType: 'comment', enabled: false },
+  { notificationType: 'chat', enabled: false },
+  { notificationType: 'follow', enabled: false },
+  { notificationType: 'receive', enabled: false },
+  { notificationType: 'followuserpost', enabled: false },
+  { notificationType: 'newuser', enabled: false },
+];
+
 export function useNotificationSettings() {
   return useQuery<NotificationSetting[]>({
     queryKey: ['notificationSettings'],
     queryFn: getNotificationsSettingStatus,
+    placeholderData: defaultSettings,
     staleTime: 1000 * 60 * 5,
   });
 }
