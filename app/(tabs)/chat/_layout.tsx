8f307efaa765f223a1336242575e8e176a658914
shinhm1 <shinhm1@naver.com>
2025-10-29 17:11:37 +0900

    feat: 알림 딥링크 initialRoute 추가

--- app/(tabs)/chat/_layout.tsx
+++ app/(tabs)/chat/_layout.tsx
@@ -1,5 +1,10 @@
 import { Stack } from 'expo-router';
 
+// deeplink 클릭 시 먼저 initialRoute로 이동
+export const unstable_settings = {
+  initialRouteName: 'index',
+};
+
 export default function ChatLayout() {
   return (
     <Stack>
@@ -12,6 +17,13 @@ export default function ChatLayout() {
       />
       <Stack.Screen
         name="CreateSpaceScreen"
+        options={{
+          headerShown: false,
+          title: '채팅',
+        }}
+      />
+      <Stack.Screen
+        name="ChattingRoomScreen"
         options={{
           headerShown: false,
           title: '채팅',
